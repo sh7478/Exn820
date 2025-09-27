c4df2438a19a2dcfb3ae9e3052b4aa0f56dedfe7
sh7478 <sh7478@bs.amalnet.k12.il>
2025-09-27 16:37:08 +0300

    added photos of graphs and finished coding

--- app/src/main/java/com/example/exn820/QuadraticSolverActivity.java
+++ app/src/main/java/com/example/exn820/QuadraticSolverActivity.java
@@ -34,10 +34,51 @@ public class QuadraticSolverActivity extends AppCompatActivity {
     }
 
     public void solver(double a, double b, double c) {
-
+        if(b*b-4*a*c < 0)
+        {
+            answer1.setText("No solution");
+            answer2.setText("No solution");
+            if(a > 0)
+            {
+                graphView.setImageResource(R.drawable.graph3);
+            }
+            else
+            {
+                graphView.setImageResource(R.drawable.graph6);
+            }
+        }
+        double solution1, solution2;
+        solution1 = (-b + Math.sqrt(b*b-4*a*c))/(2*a);
+        solution2 = (-b - Math.sqrt(b*b-4*a*c))/(2*a);
+        if(solution1 == solution2)
+        {
+            answer1.setText("x = " +String.format("%.5f", solution1));
+            answer2.setText("x = " +String.format("%.5f", solution2));
+            if(a > 0)
+            {
+                graphView.setImageResource(R.drawable.graph2);
+            }
+            else
+            {
+                graphView.setImageResource(R.drawable.graph5);
+            }
+        }
+        else
+        {
+            answer1.setText("x = " +String.format("%.5f", solution1));
+            answer2.setText("x = " +String.format("%.5f", solution2));
+            if(a>0)
+            {
+                graphView.setImageResource(R.drawable.graph1);
+            }
+            else
+            {
+                graphView.setImageResource(R.drawable.graph4);
+            }
+        }
     }
 
     public void goBack(View view) {
-
+        finish();
     }
 }
